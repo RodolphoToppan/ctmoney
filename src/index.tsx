5f17a66269ff7beda7d03b93aaa360c126a808bd
Rodolpho Toppan <rodolphotoppan@hotmail.com>
2022-08-18 08:21:51 -0300

    day  18/08/2022

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
